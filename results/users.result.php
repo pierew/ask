b6f8f088686605c463015640a939e05073058490
Piere Wöhl <woehlpiere@googlemail.com>
2015-01-23 09:44:07 +0000

    Bugfixing & Results

--- results/users.result.php
+++ results/users.result.php
@@ -1,9 +1,24 @@
 <?php
 $userResults = getResultOfUser($_GET['username']);
-echo "Ergebnisse f&uuml;r Benutzer: ".$_GET['username']." ";
+echo "Ergebnisse f&uuml;r Benutzer: <b>".$_GET['username']." <br></b>";
 for ($i = 0; $i < sizeof($userResults); $i++) {
-    echo $userResults[$i]['answer'];
+    
+    
+    if($userResults[$i]['answer'] < 5)
+    {
+        $zustimmung = "(Stimmt weniger zu)";
+    }
+    else if($userResults[$i]['answer'] == 5)
+    {
+        $zustimmung = "(Neutral)";
+    }
+    else if($userResults[$i]['answer'] == 6)
+    {
+        $zustimmung = "(Neutral)";
+    }
+    else if($userResults[$i]['answer'] > 6)
+    {
+        $zustimmung = "(Stimmt mehr zu)";
+    }
+    echo "<br>" . $userResults[$i]['ask_question_idask_question'] . ". " . getQuestionText($userResults[$i]["ask_question_idask_question"])  . " | Antwort: " . $userResults[$i]['answer'] . "&nbsp;&nbsp;&nbsp;&nbsp;" . $zustimmung ;
 }
-echo '<pre>';
-print_r($userResults);
-echo '</pre>';
